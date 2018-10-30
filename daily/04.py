d19236f7b09c780d58f380157e14a50f36eff1b0
tibbsm <tibbs.marc@gmail.com>
2018-10-30 13:00:12 +0900

    daily 4w

---
+++ daily/04.py
@@ -0,0 +1,30 @@
+
+# Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+
+# For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+def findLow(arr):
+
+    arr.append(0)
+    size = len(arr)
+
+    for i in range(len(arr)):
+        if arr[i] < 0 or arr[i] >= size:
+            arr[i] = 0
+
+    for i in range(len(arr)):
+        arr[arr[i]%size] += size
+
+    for i in range(1, len(arr)):
+        if arr[i]/size == 0:
+            return i
+
+    return size
+
+print findLow([3,4,-1,1])
+print findLow([1,2,0])
+print findLow([2,2,0])
+print findLow([2,2])
+
+    
+
